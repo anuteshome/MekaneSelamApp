f8b8a099053acab4fd30556ee2b45b097a9ff8fb
anuteshome <ananyateshome2@gmail.com>
2026-03-13 21:50:55 +0300

    fix: update document

--- lib/screens/pdf_screen.dart
+++ lib/screens/pdf_screen.dart
@@ -6,8 +6,9 @@ import 'package:path_provider/path_provider.dart';
 
 class PdfScreen extends StatefulWidget {
   final String path;
+  final String title;
 
-  const PdfScreen({super.key, required this.path});
+  const PdfScreen({super.key, required this.path, required this.title});
 
   @override
   State<PdfScreen> createState() => _PdfScreenState();
@@ -41,7 +42,9 @@ class _PdfScreenState extends State<PdfScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Course PDF")),
+     appBar: AppBar(
+  title: Text(widget.title),
+),
       body: localPath == null
           ? const Center(child: CircularProgressIndicator())
           : PDFView(
