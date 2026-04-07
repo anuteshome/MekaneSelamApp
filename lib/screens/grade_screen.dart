0db3bae8952e02197bbdff578dfbadde3b8cdb20
anuteshome <ananyateshome2@gmail.com>
2026-04-08 02:01:54 +0300

    add grade 2 and 3 books

--- lib/screens/grade_screen.dart
+++ lib/screens/grade_screen.dart
@@ -5,8 +5,13 @@ import 'package:mk/screens/course_screen.dart';
 class GradeScreen extends StatelessWidget {
 
   final List years= [
+"2ተኛ ክፍል",  
+"3ተኛ ክፍል",
+"4ተኛ ክፍል",
+"5ተኛ ክፍል",
 "ቀዳማይ 1",
 "ቀዳማይ 2",
+
 
 
   ];
